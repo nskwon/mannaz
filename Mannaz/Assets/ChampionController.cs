81cdd88975e641cd5863ebe954e70e46b2a98aac
nskwon <nskwon@ucsd.edu>
2019-12-17 00:45:46 -0800

    Added champion movement and updated map camera to be top down view

---
+++ Mannaz/Assets/ChampionController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ChampionController : MonoBehaviour
+{
+    public Camera camera;
+
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
