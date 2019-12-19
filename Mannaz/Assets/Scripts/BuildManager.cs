b9e991f49642e9a1dfd3ab631ab383287290d7af
vchi747 <46303803+vchi747@users.noreply.github.com>
2019-12-18 18:11:52 -0800

    Modifications to Node

---
+++ Mannaz/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager in scene");
+            return;
+        }
+        instance = this;
+
+    }
+    public GameObject standardTBuildingPrefab;
+
+    void Start()
+    {
+        buildingToBuild = standardTBuildingPrefab;
+    }
+    private GameObject buildingToBuild;
+
+    public GameObject GetBuildingToBuild()
+    {
+        return buildingToBuild;
+    }
+}
---
+++ Mannaz/Assets/Scripts/Node.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    //private GameObject building;
+    private Renderer rend;
+    private Color startColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        GameObject turretToBuild = BuildManager.instance.GetBuildingToBuild();
+        GameObject turret = (GameObject)Instantiate(turretToBuild, transform.position, transform.rotation);
+    }
+    // Update is called once per frame
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;   
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
