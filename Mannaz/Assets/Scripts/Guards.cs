1527172b8a12fea73a42a868b091e25f6518735a
Alvinw9 <33185325+Alvinw9@users.noreply.github.com>
2019-12-19 14:53:57 -0800

    Need to change Guard impact effect

--- Mannaz/Assets/Scripts/Guards.cs
+++ Mannaz/Assets/Scripts/Guards.cs
@@ -16,15 +16,111 @@ public class Guards : MonoBehaviour
     public int backward = 0;
     public bool attacking = true;
 
+    private Transform target;
+    public string enemyTag = "Enemy";
+    Quaternion initialRot;
+    Vector3 initialPos;
+    public GameObject WizardImpactEffect;
+
     void Start()
     {
+        initialRot = transform.rotation;
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
         StartCoroutine(CoUpdate());
+    }
+
+    void UpdateTarget()
+    {
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= attackRange)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+
+    }
+
+    void Update()
+    {
+
+        if (target == null)
+        {
+            transform.rotation = initialRot;
+            attacking = false;
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = lookRotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if ( pullback == 0 )
+        {
+            initialPos = transform.position;
+        }
+
+        // attacking "animation"
+        if (pullback < 20)
+        {
+            transform.position -= transform.forward * Time.deltaTime;
+            pullback++;
+        }
+        else if (forward < 9)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * 30);
+            forward++;
+        }
+        else if ( forward == 9 )
+        {
+            HitTarget();
+            forward++;
+        }
+        else if (backward < 35)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, initialPos, Time.deltaTime * 13);
+            backward++;
+        }
+        else
+        {
+
+            pullback = 0;
+            forward = 0;
+            backward = 0;
+        }
+
     }
 
     IEnumerator CoUpdate()
     {
         while (true)
         {
+
+            if (target == null)
+            {
+                yield return null;
+            }
+            else
+            {
+                attacking = true;
+            }
 
             if (health <= 0)
             {
@@ -36,50 +132,24 @@ public class Guards : MonoBehaviour
             if (!attacking)
             {
                 transform.position += transform.forward * Time.deltaTime * speed;
-                attackDelay++;
-
-                if (attackDelay >= 30)
-                {
-                    attacking = true;
-                    attackDelay = 0;
-                    yield return new WaitForSeconds(0.5f);
-                }
-
-            }
-
-            // initiate attack
-            else
-            {
-                // attacking "animation"
-                if (pullback < 10)
-                {
-                    transform.position -= transform.forward * Time.deltaTime;
-                    pullback++;
-                }
-                else if (forward < 3)
-                {
-                    transform.position += transform.forward * Time.deltaTime * 30;
-                    forward++;
-                }
-                else if (backward < 6)
-                {
-                    transform.position -= transform.forward * Time.deltaTime * 13;
-                    backward++;
-                }
-                else
-                {
-                    yield return new WaitForSeconds(1f);
-                    attacking = false;
-                    pullback = 0;
-                    forward = 0;
-                    backward = 0;
-                }
             }
 
             yield return null;
 
         }
 
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+
+    void HitTarget()
+    {
+        GameObject effectIns = (GameObject)Instantiate(WizardImpactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 1.5f);
     }
 
 }
